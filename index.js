35c14fffc395f1939048c0e45177217314e7376c
joseluismunozzuta <joseluismunozzuta@gmail.com>
2021-07-20 14:49:14 -0500

    avance anthony

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ server.listen(3000,function () {
    console.log("Servidor levantado exitosamente");
 });
 app.get("/login",function(req,res){
-    res.sendFile(__dirname+"/login.html");
+    res.sendFile(__dirname+"/index.html");
 });
 
 let conn = mysql.createConnection({
@@ -27,34 +27,55 @@ conn.connect(function(err){
     console.log("Conexión exitosa a base de datos");
 });
 
-let nombres=[];
+let nombres=["Mario","Luis"];
 let cantidad=0;
-let clienteName="";
+let clienteName="Mario";
 let user="";
 let pass="";
 io.on("connection",function (socket) {
    cantidad++;
    io.emit("cantidad",cantidad);
    console.log("usuario conectado");
+
+    var index= nombres.indexOf(clienteName);
+    if (index != -1) {
+        nombres.splice(index, 0);
+    }
+    io.emit("usuarios",nombres);
+
    socket.on("disconnect",function () {
        cantidad--;
        io.emit("cantidad",cantidad);
-       var index= nombres.indexOf(clienteName);
-       if (index != -1) {
-           nombres.splice(index, 1);
-       }
-       io.emit("usuarios",nombres);
        console.log("usuario desconectado");
    });
 
+   socket.on("ultimos5mensajes", function(mensaje){
+       
+    });
+   
    socket.on("textoChat",function(mensaje){
        if(!(mensaje == "cmd-mensajes")){
            if(!(mensaje == "cmd-usuarios-c")){
                if(!((mensaje.includes('cmd-')))){
                    console.log("No es ningun tipo de comando");
-                   let info="mensaje de "+clienteName+ ": "+ mensaje;
-                   console.log(info);
-                   socket.broadcast.emit("mensaje",info);
+                    let date_ob = new Date();
+                    let date = ("0" + date_ob.getDate()).slice(-2);
+                    let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+                    let year = date_ob.getFullYear();
+                    let hours = date_ob.getHours();
+                    let minutes = date_ob.getMinutes();
+                    let seconds = date_ob.getSeconds();
+                   let fechayhora = year + "-" + month + "-" + date + " " + hours + ":" + minutes + ":" + seconds;
+                   var sql = "INSERT INTO `efinal_g5`.`mensajes` (`textomensaje`, `hora`, `usuarios_idusuarios`) VALUES (?, ?, ?)";
+                   var params = [mensaje,fechayhora, 2];
+                   conn.query(sql,params, function (err,results){
+                       if (err) throw err;
+                       console.log("Guardado exitoso");
+                   });
+                   console.log(fechayhora);
+                   let mensajechat="Usuario:" + clienteName+ "/" + "Fecha y hora del mensaje: " + fechayhora + "/" + "Texto del mensaje: " + mensaje;
+                   console.log(mensajechat);
+                   io.emit("mensajechat",mensajechat);
                }else{
                    console.log("Comando de cmd-palabra");
                    let mensaje_cortado = mensaje.substr(4,mensaje.length);
